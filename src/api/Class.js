3d2a2a72b3ff0dcff5c89da6bfdff7f599ee3e53
志昀 陳 <justin020806@gmail.com>
2022-01-04 22:56:12 +0800

    班級管理API 基本

--- src/api/Class.js
+++ src/api/Class.js
@@ -1,9 +1,46 @@
 import request from '@/utils/request'
+// 班級管理系統API
 
-export function GetClassesData(TeacherNumber) {
+// 全班級列表
+export function getClassList() {
+  return request({
+    url: '',
+    method: 'get'
+  })
+}
+
+// 單班級詳細資料
+export function getClasses(ClassID) {
   return request({
     url: '/vue-element-admin/article/list',
     method: 'get',
-    params: TeacherNumber
+    params: ClassID
+  })
+}
+
+// 創建班級資料
+export function createClass(data) {
+  return request({
+    url: '',
+    method: 'post',
+    data
+  })
+}
+
+// 更新班級資料
+export function updateClass(data, ClassID) {
+  return request({
+    url: '',
+    method: 'put',
+    params: { ClassID },
+    data
+  })
+}
+
+// 刪除班級資料
+export function deleteClass(ClassID) {
+  return request({
+    url: '',
+    method: 'post'
   })
 }
